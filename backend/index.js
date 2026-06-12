99ea454cbdee4fd7fcc1cab2d9aba12db2ccc797
Smriti Patel <smritiiipatel@gamil.com>
2026-06-13 00:36:25 +0530

    fix: dynamic PORT for Render

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,9 @@ import './src/config/env.js'
 import connectDB from './src/config/database.js';
 import app from "./src/app.js";
 
-
 connectDB();
 
-
-app.listen(3000,()=>{
-    console.log("server is running");
-
-    
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`);
 })
